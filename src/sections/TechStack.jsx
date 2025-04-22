768205b7a35809eb61439af5b7f605111edd114c
Piotr Deregowski <deregowskipiotr@interia.pl>
2025-04-22 15:02:55 +0200

    TechStack component with animation icons DONE

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,9 +1,57 @@
-
+import { useGSAP } from '@gsap/react';
+import TechIcon from '../components/Models/TechLogos/TechIcon.jsx';
+import TitleHeader from '../components/TitleHeader.jsx';
+import { techStackIcons } from "../constants/index.js";
+import { gsap } from 'gsap';
 
 const TechStack = () => {
+  useGSAP(() => {
+    gsap.fromTo('.tech-card', {
+      opacity: 0,
+      y: 50,
+    }, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: 'power2.inOut',
+      stagger: 0.4,
+      scrollTrigger: {
+        trigger: '#skills',
+        start: "top center",
+      }
+    });
+  })
+
   return (
-    <div>TechStack</div>
-  )
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="My Preferred Tech Stack"
+          sub="🤝 The Skills I Bring to the table. "
+        />
+
+        <div className="tech-grid">
+          {techStackIcons.map((icon) => (
+            <div
+              key={icon.name}
+              className="card-border tech-card overflow-hidden group xl:rounded-full rouded-lg"
+            >
+              <div className="tech-card-animated-bg" />
+              <div className="tech-card-content">
+                <div className="tech-icon-wrapper">
+                  <TechIcon model={icon} />
+                </div>
+
+                <div className="padding-x w-full">
+                  <p>{icon.name}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TechStack
